b7b0039a9d6c460d54ef0677d707edb360fa6b35
shehryarlodhi <lodhishehryarkhan@gmail.com>
2022-11-29 09:59:53 +0500

    deploy my app

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Dummy React App</h1>
+      
+        <table>
+
+        <tr>
+        <td>
+          Name:
+        </td>
+        <td>
+          Shehryar Sajid
+        </td>
+        </tr>
+
+        <tr>
+        <td>
+          Roll No:
+        </td>
+        <td>
+          20I-1771
+        </td>
+        </tr>
+        <tr>
+        <td>
+          Degree:
+        </td>
+        <td>
+          Software Engineering
+        </td>
+        </tr>
+      </table>
+      
+      
+    </div>
+  );
+}
+
+export default App;
